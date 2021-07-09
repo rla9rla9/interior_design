7e49450ed8531eb3ee4626116d45dbe65ccff0c8
rla9rla9 <76562349+rla9rla9@users.noreply.github.com>
2021-07-09 15:50:33 +0900

    first commit

---
+++ src/js/website.js
@@ -0,0 +1,12 @@
+/* $('dt').on('click', function () {
+
+  if ($(this).hasClass('on')) {
+      slideUp();
+  } else {
+      slideUp();
+      $(this).addClass('on').next().slideDown();
+  }
+  function slideUp() {
+      $('dt').removeClass('on').next().slideUp();
+  };
+}) */
